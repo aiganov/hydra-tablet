b1528c21a927d3aebada36f721f886a758ce5667
aiganov <man-simple@mail.ru>
2020-06-14 22:45:30 +0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,31 @@
+//$('.slider').slick({
+ // slidesToShow: 1,
+  //slidesToScroll: 1
+//});
+
+
+$(".custom-scroll").niceScroll({
+	cursorcolor:"#e7e7e7",
+	hidecursordelay: 550,
+	cursorfixedheight: 118,
+	cursorwidth: "24px"
+
+}); 
+
+$(".second-scroll").niceScroll({
+	cursorcolor:"#e7e7e7",
+	hidecursordelay: 550,
+	cursorfixedheight: 118,
+	cursorwidth: "24px",
+	horizrailenabled: false
+
+}); 
+
+
+$('.slider3').mobilyslider({
+    transition: 'fade',
+    animationSpeed: 300,
+    bullets: false,
+    arrows: true,
+    arrowsHide: false
+});
